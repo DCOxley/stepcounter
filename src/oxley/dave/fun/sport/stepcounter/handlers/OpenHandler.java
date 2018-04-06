6156d8f4b068db79431157509ec1c85f37a7a0b4
oxleyd <oxleyd@oxleyd-LIFEBOOK-E782.(none)>
2018-04-06 22:11:09 +0200

    First commit

---
+++ src/oxley/dave/fun/sport/stepcounter/handlers/OpenHandler.java
@@ -0,0 +1,13 @@
+package oxley.dave.fun.sport.stepcounter.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+public class OpenHandler {
+
+	@Execute
+	public void execute(Shell shell){
+		FileDialog dialog = new FileDialog(shell);
+		dialog.open();
+	}
+}
