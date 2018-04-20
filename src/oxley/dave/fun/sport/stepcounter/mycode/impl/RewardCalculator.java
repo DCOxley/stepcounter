acdbce21e85f4881108e3c0f187f748a6b5dbce9
oxleyd <oxleyd@oxleyd-LIFEBOOK-E782>
2018-04-20 11:14:59 +0200

    basic reward calculator

--- src/oxley/dave/fun/sport/stepcounter/mycode/impl/RewardCalculator.java
+++ src/oxley/dave/fun/sport/stepcounter/mycode/impl/RewardCalculator.java
@@ -1,5 +1,17 @@
 package oxley.dave.fun.sport.stepcounter.mycode.impl;
 
+import java.util.List;
+
+import oxley.dave.fun.sport.stepcounter.impl.CycleImpl;
+
 public class RewardCalculator {
+	private List<CycleImpl> cycles;
+	private boolean wasSuccessful;
+
+	public RewardCalculator(List<CycleImpl> cycles, boolean wasSuccessful) {
+		super();
+		this.cycles = cycles;
+		this.wasSuccessful = wasSuccessful;
+	}
 
 }
