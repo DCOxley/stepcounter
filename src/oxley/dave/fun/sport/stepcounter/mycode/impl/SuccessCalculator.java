f95fffbe15a0ec32152f68229ec2c63b9bd94cd4
oxleyd <oxleyd@oxleyd-LIFEBOOK-E782>
2018-04-20 10:47:46 +0200

    constructor for successcalculator

--- src/oxley/dave/fun/sport/stepcounter/mycode/impl/SuccessCalculator.java
+++ src/oxley/dave/fun/sport/stepcounter/mycode/impl/SuccessCalculator.java
@@ -1,5 +1,22 @@
 package oxley.dave.fun.sport.stepcounter.mycode.impl;
 
+import java.util.List;
+
+import oxley.dave.fun.sport.stepcounter.impl.DayImpl;
+
 public class SuccessCalculator {
+
+	private List<DayImpl> days;
+	private boolean wasSuccesful;
+
+	public SuccessCalculator(List<DayImpl> days, boolean wasSuccesful) {
+		super();
+		this.days = days;
+		this.wasSuccesful = wasSuccesful;
+	}
+
+	private boolean wasPeriodSuccessful() {
+		return wasSuccesful;
+	}
 
 }
