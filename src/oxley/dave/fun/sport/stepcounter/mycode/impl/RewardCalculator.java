5eb53f608fab914614036196abe44442705aa582
oxley_d <dcoxley@googlemail.com>
2018-05-01 16:20:27 +0200

    test build

--- src/oxley/dave/fun/sport/stepcounter/mycode/impl/RewardCalculator.java
+++ src/oxley/dave/fun/sport/stepcounter/mycode/impl/RewardCalculator.java
@@ -1,6 +1,18 @@
 package oxley.dave.fun.sport.stepcounter.mycode.impl;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
 
 import oxley.dave.fun.sport.stepcounter.api.Period;
 import oxley.dave.fun.sport.stepcounter.api.Status;
@@ -19,7 +31,122 @@ public class RewardCalculator implements ICalculator {
 	@Override
 	public Status calculate(Period p) {
 		// TODO Auto-generated method stub
-		return null;
+		return new Status() {
+			
+			@Override
+			public void eSetDeliver(boolean deliver) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void eNotify(Notification notification) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean eDeliver() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public EList<Adapter> eAdapters() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void eUnset(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void eSet(EStructuralFeature feature, Object newValue) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Resource eResource() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public boolean eIsSet(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean eIsProxy() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Object eInvoke(EOperation operation, EList<?> arguments) throws InvocationTargetException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Object eGet(EStructuralFeature feature, boolean resolve) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Object eGet(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EList<EObject> eCrossReferences() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EList<EObject> eContents() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EReference eContainmentFeature() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EStructuralFeature eContainingFeature() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EObject eContainer() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EClass eClass() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public TreeIterator<EObject> eAllContents() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
 	}
 
 }
